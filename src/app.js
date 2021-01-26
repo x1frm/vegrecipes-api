e2f81d0706aada932cc26227eb8aee32ab3717b5
x1frm <alex-shvets@yandex.ru>
2021-01-26 23:30:47 +0300

    init: Mongoose and env vars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to vegrecipes!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+process.on('uncaughtException', (err, origin) => {
+  console.log(`Caught exception: ${err}\n Exception origin: ${origin}`);
 });
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, '\nreason:', reason);
+});
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,27 @@
+// const logger = require('./common/logging');
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
+const app = require('./app');
+
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+// db.on('error', () => logger.error('MongoDB connection error:')).once(
+//   'open',
+//   () => {
+//     logger.info('Successfully connect to DB');
+//     app.listen(PORT, () =>
+//       logger.info(`App is running on http://localhost:${PORT}`)
+//     );
+//   }
+// );
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+});
