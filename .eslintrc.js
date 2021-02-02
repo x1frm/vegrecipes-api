05644610b0638bcfb917bf7a11126b02692ac860
x1frm <alex-shvets@yandex.ru>
2021-02-02 10:47:29 +0300

    init: ESLint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,40 @@ module.exports = {
     es2021: true,
     'jest/globals': true,
   },
-  extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:jest/style'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint', 'jest', 'import'],
   rules: {
     'no-console': 'off',
     'arrow-parens': [2, 'as-needed'],
+    'import/extensions': [2, 'never'],
+    'jest/expect-expect': 0,
   },
   settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', './'],
+        paths: ['./'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: 'tsconfig.json',
       },
     },
   },
--- test/welcome.test.ts
+++ test/welcome.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../src/app.ts';
+import app from '../src/app';
 
 describe('Test the root path', () => {
   test('It should response the GET method', () => request(app).get('/').expect(200));
