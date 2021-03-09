08563fdd275776dcd02598a6b92c81fa4ed1b5df
x1frm <alex-shvets@yandex.ru>
2021-03-09 21:35:48 +0300

    feat: Equipment model

--- src/resources/equipment/equipment.model.ts
+++ src/resources/equipment/equipment.model.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import { EquipmentDocument, EquipmentModel, EquipmentSchema } from 'src/interfaces/mongoose.gen';
+import { getSimilarStringValidator, nameSetter } from '../../common/model.utils';
 
 const equipmentSchema: EquipmentSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     maxlength: 100,
+    set: nameSetter,
+    validate: getSimilarStringValidator('name'),
   },
 });
 
---
+++ src/resources/equipment/equipment.test.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import { DeepPartial } from 'src/common/types';
+import { Equipment as IEquipment } from 'src/interfaces/mongoose.gen';
+import { LongStr } from '../../common/utils';
+import { dbInit, dbClose } from '../../../test/setup/shared.config';
+import Equipment from './equipment.model';
+
+const equipmentData: DeepPartial<IEquipment> = {
+  name: 'Blender',
+};
+
+describe('Equipment Model Test', () => {
+  beforeAll(dbInit);
+  afterAll(dbClose);
+
+  it('creates and saves equipment', async () => {
+    expect.assertions(3);
+    const equipment = new Equipment(equipmentData);
+    const result = await equipment.save();
+    expect(result._id).toBeDefined();
+    expect(result.isNew).toBe(false);
+    expect(result.toObject()).toMatchObject(equipmentData);
+  });
+
+  it('fails saving if required field is not provided', async () => {
+    expect.assertions(2);
+    const incorrect = { ...equipmentData };
+    delete incorrect.name;
+    const equipment = new Equipment(incorrect);
+
+    const promise = equipment.save();
+    await Promise.all([
+      expect(promise).rejects.toBeInstanceOf(mongoose.Error.ValidationError),
+      expect(promise).rejects.toHaveProperty('errors.name'),
+    ]);
+  });
+
+  it("fails saving if data don't pass mongoose validation", async () => {
+    expect.assertions(2);
+
+    const incorrect: DeepPartial<IEquipment> = {
+      ...equipmentData,
+      name: LongStr(101),
+    };
+
+    const equipment = new Equipment(incorrect);
+
+    const promise = equipment.save();
+    await Promise.all([
+      expect(promise).rejects.toBeInstanceOf(mongoose.Error.ValidationError),
+      expect(promise).rejects.toHaveProperty('errors.name'),
+    ]);
+  });
+});
