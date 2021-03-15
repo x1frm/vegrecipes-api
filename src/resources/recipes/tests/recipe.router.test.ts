76b169f5235f36052ec819588bffad48905d8973
x1frm <alex-shvets@yandex.ru>
2021-03-15 23:15:09 +0300

    feat: Recipe router tests

---
+++ src/resources/recipes/tests/recipe.router.test.ts
@@ -0,0 +1,82 @@
+import supertest from 'supertest';
+import { clearDatabase } from '../../../../test/setup/db';
+import app from '../../../app';
+import { RecipeDto } from '../../../interfaces/dto';
+import { getRecipeData } from './helpers';
+
+const request = supertest(app);
+const url = '/api/recipes/';
+
+const recipe = getRecipeData();
+const postOne = (item: RecipeDto = recipe) => request.post(url).send(item).expect(200);
+
+describe('/recipes/', () => {
+  afterEach(clearDatabase);
+
+  it('Gets empty array when no recipes were added yet', async () =>
+    request.get(url).expect(200, []));
+
+  it('Gets all recipes', async () => {
+    expect.assertions(1);
+    const pancakes = {
+      ...getRecipeData(),
+      name: 'Pancakes',
+    };
+    await postOne();
+    await postOne(pancakes);
+
+    const get = await request.get(url).expect(200);
+    expect(get.body).toMatchObject([recipe, pancakes]);
+  });
+
+  it('Post a single recipe and gets it back', async () => {
+    expect.assertions(2);
+
+    const post = await postOne();
+    expect(post.body).toMatchObject(recipe);
+
+    const get = await request.get(url).expect(200);
+    expect(get.body).toMatchObject([recipe]);
+  });
+
+  it('Gets a recipe by id', async () => {
+    expect.assertions(1);
+
+    const post = await postOne();
+
+    const get = await request.get(`${url}${(post.body as RecipeDto)._id}`).expect(200);
+    expect(get.body).toMatchObject(recipe);
+  });
+
+  it('Puts a recipe', async () => {
+    expect.assertions(2);
+    const post = await postOne();
+
+    const updated = getRecipeData();
+    updated.description = 'Tasty breakfast';
+
+    const put = await request
+      .put(`${url}${(post.body as RecipeDto)._id}`)
+      .send(updated)
+      .expect(200);
+    expect(put.body.description).toBe(updated.description);
+
+    const get = await request.get(`${url}${(put.body as RecipeDto)._id}`).expect(200);
+    expect(get.body).toMatchObject(updated);
+  });
+
+  it('Deletes a recipe', async () => {
+    const post = await postOne();
+    return request.del(`${url}${(post.body as RecipeDto)._id}`).expect(204);
+  });
+
+  // it('Responses with 404 if id is not found', async () => {
+  //   const id = '604bdc0e9d07d51b052f872f';
+  //   await request.get(url + id).expect(404);
+  //   await request
+  //     .put(url + id)
+  //     .send(recipe)
+  //     .expect(404);
+  //   await request.delete(url + id).expect(404);
+  // });
+});
